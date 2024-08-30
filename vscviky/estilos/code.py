11b85611baf9c7687846589ad545628d104bccfe
victoriaNarvaez <victorinarvaez159@gmail.com>
2024-08-29 22:34:15 -0500

    Proyecto

---
+++ vscviky/estilos/code.py
@@ -0,0 +1,9 @@
+print("Hola Mundo")
+a = input("Dijite a: ")
+b = input("Dijite b: ")
+if (a > b):
+    print("Mayor", a)
+else:
+    print("Menor", a)
+mom = input("Dijite su ñombre: ")
+print("Su ñombre es: ", mom)
